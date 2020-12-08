37eb4e20c903dc2dfd1de42acd41ffa29d1bc82f
Bojan Gabric <bojan@bojangabric.com>
2020-12-08 03:53:06 +0100

    Add SEO config

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+const title = 'Best Hotels & Accommodations';
+const description = '';
+
+const SeoConfig = {
+  title,
+  description,
+  titleTemplate: '%s - Hotello'
+};
+
+export default SeoConfig;
