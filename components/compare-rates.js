6ad0d3ffb245eba69554fcb9a7a558cd968e2e5e
Bojan Gabric <bojan@bojangabric.com>
2020-12-08 03:55:51 +0100

    Add compare rates

---
+++ components/compare-rates.js
@@ -0,0 +1,42 @@
+const CompareRates = () => (
+  <div className="flex items-center justify-between w-4/5 mx-auto lg:space-x-32 lg:w-9/12">
+    <div className="relative w-full space-y-4 lg:space-y-8 lg:w-1/2">
+      <div className="text-2xl md:text-3xl font-semibold md:leading-none">
+        Compare Rates
+        <br />& Save Up to 80 %
+      </div>
+      <div className="md:text-lg leading-normal text-gray-900">
+        So you’re going abroad, you’ve chosen your destination and now you have to choose a hotel. Ten years ago, you’d
+        have probably visited your local trave agent The 21st Century way to select and book your hotel is of course on
+        the internet.
+      </div>
+      <a
+        href="#!"
+        className="inline-flex items-center px-6 py-3 md:text-lg text-white bg-blue-500 rounded hover:bg-blue-600"
+      >
+        <div>Compare now</div>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+            <path d="M14.59 13H7a1 1 0 0 1 0-2h7.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4l2.3-2.3z" />
+          </svg>
+        </div>
+      </a>
+      <div className="flex items-center pt-10 lg:justify-between xl:w-4/5">
+        <div className="pr-4 sm:pr-8 xl:pr-6 md:w-1/5 lg:w-full">
+          <img src="/images/getaroom.png" />
+        </div>
+        <div className="pr-4 sm:pr-8 xl:pr-6 md:w-1/5 lg:w-full">
+          <img src="/images/expedia.png" />
+        </div>
+        <div className="md:w-1/5 lg:w-full">
+          <img src="/images/hotels.png" />
+        </div>
+      </div>
+    </div>
+    <div className="hidden w-1/2 lg:block">
+      <img className="object-cover w-full rounded h-120" src="/images/beach.jpg" />
+    </div>
+  </div>
+);
+
+export default CompareRates;
