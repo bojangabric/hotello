b492f2c5ad16088d3255147d76ccf98ba396f114
Bojan Gabric <bojan@bojangabric.com>
2020-12-08 03:56:28 +0100

    Add trending-hotels

---
+++ components/trending-hotels/hotel.js
@@ -0,0 +1,35 @@
+const Hotel = ({ city, country, image, price, stars }) => (
+  <div className="">
+    <div className="relative overflow-hidden bg-white rounded shadow">
+      <img className="object-cover object-center w-full h-80" src={image} />
+    </div>
+    <div className="flex items-center justify-between px-1">
+      <div>
+        <div className="mt-1 text-gray-600 text-sm">{country}</div>
+        <div className="mt-px mb-1 font-medium text-gray-800 text-xl leading-tight">{city}</div>
+        <div className="flex">
+          {[...Array(stars)].map(() => (
+            <svg
+              className="h-5 w-5 text-yellow-300 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+          ))}
+          {[...Array(5 - stars)].map(() => (
+            <svg className="h-5 w-5 text-gray-200 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+          ))}
+        </div>
+      </div>
+      <div className="text-blue-500 text-right">
+        <div className="text-xl leading-snug">${price}</div>
+        <div className="leading-none">per day</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Hotel;
---
+++ components/trending-hotels/trending-hotels.js
@@ -0,0 +1,29 @@
+import Hotel from './hotel';
+import hotels from '../../database/hotels.json';
+import Link from 'next/link';
+
+const TrendingHotels = () => (
+  <div className="font-medium">
+    <div className="text-3xl text-center">
+      <div className="w-24 h-1 mx-auto mb-4 border-4 border-b border-blue-500"></div>
+      <div className="px-12">Trending Hotels Right Now</div>
+    </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-16 sm:gap-8 w-4/5 pt-24 mx-auto lg:w-9/12">
+      {hotels.slice(0, 4).map(hotel => (
+        <Hotel {...hotel} />
+      ))}
+    </div>
+    <div className="mt-16 text-center">
+      <Link href={'/hotels'}>
+        <a className="inline-flex items-center px-6 py-3 sm:text-lg text-white bg-blue-500 rounded hover:bg-blue-600">
+          <span>Check out more</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+            <path d="M14.59 13H7a1 1 0 0 1 0-2h7.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4l2.3-2.3z" />
+          </svg>
+        </a>
+      </Link>
+    </div>
+  </div>
+);
+
+export default TrendingHotels;
