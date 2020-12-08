2d44c7010ca3e6371eb7516f72d28988e008e6b2
Bojan Gabric <bojan@bojangabric.com>
2020-12-08 03:53:38 +0100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: {
-    content: ['./components/**/*.js', './pages/**/*.js']
+    content: ['./components/**/*.js', './pages/**/*.{js,mdx}', './layouts/**/*.js', './next.config.js']
   },
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        gray: colors.blueGray
+      },
+      spacing: {
+        120: '30rem'
+      }
+    }
+  },
+  variants: {
     extend: {}
   },
-  variants: {},
-  plugins: []
+  plugins: [require('@tailwindcss/forms')]
 };
