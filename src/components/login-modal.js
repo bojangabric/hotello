8ecce040903c5ba17b7f41d85a21c96748808b75
Bojan Gabric <bojan@bojangabric.com>
2021-07-16 14:22:27 +0200

    feat: use headlessui modal

--- src/components/login-modal.js
+++ src/components/login-modal.js
@@ -1,107 +1,136 @@
-const LoginModal = ({ show, toggle }) => (
-  <div
-    className={`${
-      show ? 'table' : 'hidden'
-    } modal-mask fixed top-0 left-0 w-full h-full min-h-screen text-gray-700 z-50`}
-  >
-    <div className="table-cell align-middle">
-      <div className="modal-container py-5 px-6 lg:rounded w-full h-full md:h-auto overflow-x-hidden overflow-y-visible bg-white mx-auto">
-        <div className="flex flex-col flex-1 justify-center relative">
-          <button onClick={toggle} className="absolute top-0 right-0 focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-current w-8 h-8">
-              <path d="M15.78 14.36a1 1 0 01-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 11-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 011.42-1.42l2.83 2.83 2.82-2.83a1 1 0 011.42 1.42l-2.83 2.83 2.83 2.82z" />
-            </svg>
-          </button>
-          <div className="w-full mt-4">
-            <form className="mx-auto" method="POST">
-              <div className="flex flex-col mt-4">
-                <label
-                  className="block uppercase tracking-wider text-gray-700 text-xs font-semibold mb-2"
-                  htmlFor="email"
-                >
-                  E-Mail Address
-                </label>
-                <input
-                  className="text-gray-600 mt-1 block w-full rounded border-gray-200 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-3"
-                  name="email"
-                  id="email"
-                  type="email"
-                  placeholder="jane@example.com"
-                  required
-                />
-              </div>
-              <div className="flex flex-col mt-6">
-                <label
-                  className="block uppercase tracking-wider text-gray-700 text-xs font-semibold mb-2"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  className="text-gray-600 mt-1 block w-full rounded border-gray-200 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-3"
-                  name="password"
-                  id="password"
-                  type="password"
-                  placeholder="******************"
-                  required
-                />
-              </div>
-              <div className="flex mt-6">
-                <label className="inline-flex items-center">
-                  <input
-                    type="checkbox"
-                    className="mr-1 rounded border-gray-200 text-blue-600 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                    name="remember"
-                    id="remember"
-                  />
-                  <span className="ml-2 text-sm text-gray-700">Remember Me</span>
-                </label>
-              </div>
-              <div className="flex sm:block sm:text-center md:flex flex-col mt-6">
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-20 rounded"
-                >
-                  Login
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
+import { XIcon } from '@heroicons/react/solid';
+
+const LoginModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <>
+      <button type="button" onClick={openModal} className="font-medium hover:text-white">
+        Login/Sign Up
+      </button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={closeModal}>
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+            </Transition.Child>
+            <span className="inline-block h-screen align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md px-8 py-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
+                <button onClick={closeModal} className="absolute top-0 right-0 mr-10 mt-4">
+                  <XIcon className="h-5 w-5 text-gray-500 hover:text-gray-800 transition" />
                 </button>
+                <form className="" method="POST">
+                  <div className="flex flex-col mt-4">
+                    <label
+                      className="block uppercase tracking-wider text-gray-700 text-xs font-semibold mb-2"
+                      htmlFor="email"
+                    >
+                      E-Mail Address
+                    </label>
+                    <input
+                      className="text-gray-600 mt-1 block w-full rounded border-gray-200 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-3"
+                      name="email"
+                      id="email"
+                      type="email"
+                      placeholder="jane@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col mt-6">
+                    <label
+                      className="block uppercase tracking-wider text-gray-700 text-xs font-semibold mb-2"
+                      htmlFor="password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      className="text-gray-600 mt-1 block w-full rounded border-gray-200 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-3"
+                      name="password"
+                      id="password"
+                      type="password"
+                      placeholder="******************"
+                      required
+                    />
+                  </div>
+                  <div className="flex mt-6">
+                    <label className="inline-flex items-center">
+                      <input
+                        type="checkbox"
+                        className="mr-1 rounded border-gray-200 text-blue-600 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                        name="remember"
+                        id="remember"
+                      />
+                      <span className="ml-2 text-sm text-gray-700">Remember Me</span>
+                    </label>
+                  </div>
+                  <div className="flex sm:block sm:text-center md:flex flex-col mt-6">
+                    <button
+                      type="submit"
+                      className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-3 rounded"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+                <div className="text-center mt-4">
+                  <a href="#!" className="no-underline hover:underline text-blue-dark text-xs">
+                    Forgot Your Password?
+                  </a>
+                </div>
+                <div className="text-center mx-auto text-sm text-gray-500 my-10 w-full sm:w-4/5 md:w-full border-b border-gray-300 leading-[0.1em]">
+                  <span className="bg-white px-3">or</span>
+                </div>
+                <div className="flex sm:justify-center md:justify-between flex-wrap sm:flex-no-wrap pb-20 md:pb-4">
+                  <div className="w-full sm:w-auto mb-6 sm:mb-0">
+                    <div className="flex justify-center items-center bg-red-500 hover:bg-red-600 cursor-pointer text-white rounded px-6 py-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" className="fill-current h-5 w-5 mr-2">
+                        <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z" />
+                      </svg>
+                      <div className="font-medium">Sign in with Google</div>
+                    </div>
+                  </div>
+                  <div className="py-3 border-gray-200 border bg-gray-100 text-gray-500 px-3 rounded flex flex-col justify-center mr-6 md:mr-0 sm:mx-6 md:mx-0 hover:bg-gray-200 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-current h-5 w-5">
+                      <path d="M20.766 0H3.234A3.236 3.236 0 000 3.234v17.532A3.236 3.236 0 003.234 24h8.645l.016-8.578H9.668a.527.527 0 01-.527-.524l-.008-2.761a.522.522 0 01.523-.528h2.223V8.938c0-3.102 1.894-4.79 4.66-4.79h2.27c.289 0 .527.235.527.528v2.328a.528.528 0 01-.527.527h-1.391c-1.504 0-1.797.715-1.797 1.766v2.312h3.305c.316 0 .558.274.523.586l-.328 2.766a.527.527 0 01-.523.46h-2.961L15.62 24h5.145A3.236 3.236 0 0024 20.766V3.234A3.236 3.236 0 0020.766 0zm0 0" />
+                    </svg>
+                  </div>
+                  <div className="py-3 border-gray-200 border bg-gray-100 text-gray-500 px-3 rounded flex flex-col justify-center hover:bg-gray-200 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-current h-5 w-5">
+                      <path d="M512 97.248c-19.04 8.352-39.328 13.888-60.48 16.576 21.76-12.992 38.368-33.408 46.176-58.016-20.288 12.096-42.688 20.64-66.56 25.408C411.872 60.704 384.416 48 354.464 48c-58.112 0-104.896 47.168-104.896 104.992 0 8.32.704 16.32 2.432 23.936-87.264-4.256-164.48-46.08-216.352-109.792-9.056 15.712-14.368 33.696-14.368 53.056 0 36.352 18.72 68.576 46.624 87.232-16.864-.32-33.408-5.216-47.424-12.928v1.152c0 51.008 36.384 93.376 84.096 103.136-8.544 2.336-17.856 3.456-27.52 3.456-6.72 0-13.504-.384-19.872-1.792 13.6 41.568 52.192 72.128 98.08 73.12-35.712 27.936-81.056 44.768-130.144 44.768-8.608 0-16.864-.384-25.12-1.44C46.496 446.88 101.6 464 161.024 464c193.152 0 298.752-160 298.752-298.688 0-4.64-.16-9.12-.384-13.568 20.832-14.784 38.336-33.248 52.608-54.496z" />
+                    </svg>
+                  </div>
+                </div>
               </div>
-            </form>
-            <div className="text-center mt-4">
-              <a href="#!" className="no-underline hover:underline text-blue-dark text-xs">
-                Forgot Your Password?
-              </a>
-            </div>
+            </Transition.Child>
           </div>
-          <div
-            className="text-center mx-auto text-sm text-gray-500 my-10 w-full sm:w-4/5 md:w-full border-b border-gray-400"
-            style={{ lineHeight: '0.1em' }}
-          >
-            <span className="bg-white px-3">or</span>
-          </div>
-          <div className="flex sm:justify-center md:justify-between flex-wrap sm:flex-no-wrap pb-20 md:pb-4">
-            <div className="w-full sm:w-auto mb-6 sm:mb-0">
-              <div className="flex justify-center items-center bg-red-500 hover:bg-red-600 cursor-pointer text-white rounded px-6 py-3">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" className="fill-current h-5 w-5 mr-2">
-                  <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z" />
-                </svg>
-                <div>Sign in with Google</div>
-              </div>
-            </div>
-            <div className="py-3 border-gray-200 border bg-gray-100 text-gray-500 px-3 rounded flex flex-col justify-center mr-6 md:mr-0 sm:mx-6 md:mx-0 hover:bg-gray-200 cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="fill-current h-5 w-5">
-                <path d="M20.766 0H3.234A3.236 3.236 0 000 3.234v17.532A3.236 3.236 0 003.234 24h8.645l.016-8.578H9.668a.527.527 0 01-.527-.524l-.008-2.761a.522.522 0 01.523-.528h2.223V8.938c0-3.102 1.894-4.79 4.66-4.79h2.27c.289 0 .527.235.527.528v2.328a.528.528 0 01-.527.527h-1.391c-1.504 0-1.797.715-1.797 1.766v2.312h3.305c.316 0 .558.274.523.586l-.328 2.766a.527.527 0 01-.523.46h-2.961L15.62 24h5.145A3.236 3.236 0 0024 20.766V3.234A3.236 3.236 0 0020.766 0zm0 0" />
-              </svg>
-            </div>
-            <div className="py-3 border-gray-200 border bg-gray-100 text-gray-500 px-3 rounded flex flex-col justify-center hover:bg-gray-200 cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-current h-5 w-5">
-                <path d="M512 97.248c-19.04 8.352-39.328 13.888-60.48 16.576 21.76-12.992 38.368-33.408 46.176-58.016-20.288 12.096-42.688 20.64-66.56 25.408C411.872 60.704 384.416 48 354.464 48c-58.112 0-104.896 47.168-104.896 104.992 0 8.32.704 16.32 2.432 23.936-87.264-4.256-164.48-46.08-216.352-109.792-9.056 15.712-14.368 33.696-14.368 53.056 0 36.352 18.72 68.576 46.624 87.232-16.864-.32-33.408-5.216-47.424-12.928v1.152c0 51.008 36.384 93.376 84.096 103.136-8.544 2.336-17.856 3.456-27.52 3.456-6.72 0-13.504-.384-19.872-1.792 13.6 41.568 52.192 72.128 98.08 73.12-35.712 27.936-81.056 44.768-130.144 44.768-8.608 0-16.864-.384-25.12-1.44C46.496 446.88 101.6 464 161.024 464c193.152 0 298.752-160 298.752-298.688 0-4.64-.16-9.12-.384-13.568 20.832-14.784 38.336-33.248 52.608-54.496z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
 
 export default LoginModal;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,52 +1,40 @@
-import { useState } from 'react';
 import LoginModal from './login-modal';
 import Link from 'next/link';
 
-const Navbar = () => {
-  const [modal, setModal] = useState(false);
-  const toggleModal = () => {
-    setModal(!modal);
-    document.body.classList.toggle('fixed');
-    document.body.classList.toggle('overflow-y-scroll');
-  };
-
-  return (
-    <div className="bg-blue-500 text-white">
-      <LoginModal show={modal} toggle={toggleModal} />
-      <nav className="flex flex-wrap items-center justify-between w-4/5 py-8 md:py-12 mx-auto lg:w-3/5">
-        <div>
-          <Link href={'/'}>
-            <a>
-              <svg className="w-24 md:w-32 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 207.54 82.85">
-                <path d="M92.91 53A11.57 11.57 0 0084 49.43 11.51 11.51 0 0075.15 53a12.49 12.49 0 00-3.35 8.85 12.59 12.59 0 003.35 8.88A11.5 11.5 0 0084 74.37a11.6 11.6 0 008.9-3.59 12.59 12.59 0 003.34-8.91A12.48 12.48 0 0092.91 53zM89 67.51a6.64 6.64 0 01-10 0 8.72 8.72 0 01-1.85-5.64A8.61 8.61 0 0179 56.29a6.64 6.64 0 0110 0 8.55 8.55 0 011.85 5.58A8.66 8.66 0 0189 67.51zM102.5 68.27a6.18 6.18 0 001.55 4.52 6.11 6.11 0 004.55 1.58 6.87 6.87 0 004.87-1.53L112.24 69a3 3 0 01-2.31.83 2 2 0 01-1.67-.76 3.21 3.21 0 01-.59-2V54.54h4.82V50h-4.82v-6.5h-5.17V50h-3.94v4.52h3.94zM139.45 62.36a13.52 13.52 0 00-3.22-9.29 10.78 10.78 0 00-8.49-3.64 11.34 11.34 0 00-8.63 3.57 12.38 12.38 0 00-3.42 8.83 12 12 0 0012.44 12.5q6.06 0 9.64-3.45l-2.36-3.39a8.45 8.45 0 01-3.07 1.92 10.41 10.41 0 01-3.67.69 7.41 7.41 0 01-5.26-1.87 7.2 7.2 0 01-2.31-4.67h18.35zM121.05 60a7.2 7.2 0 011.95-4.45 6.1 6.1 0 014.77-1.89 6.18 6.18 0 014.89 1.92 6.8 6.8 0 011.8 4.38zM144.56 15.83h5.16v57.95h-5.16zM156.42 15.83h5.17v57.95h-5.17zM187.9 53a12.78 12.78 0 00-17.76 0 12.48 12.48 0 00-3.34 8.85 12.58 12.58 0 003.34 8.88 12.74 12.74 0 0017.76 0 12.6 12.6 0 003.35-8.91A12.49 12.49 0 00187.9 53zM184 67.51a6.14 6.14 0 01-5 2.29 6.07 6.07 0 01-5-2.29 8.66 8.66 0 01-1.85-5.64 8.55 8.55 0 011.85-5.58 6 6 0 015-2.29 6.11 6.11 0 015 2.29 8.61 8.61 0 011.85 5.58 8.72 8.72 0 01-1.85 5.64zM203.46 35.47h4.08v.06h-4.08zM71.8 35.53h66.07v4.08H71.8z" />
-                <path d="M168.27 35.53v4.08h35.19v39.15H66.34V10l-2.27-1.62V5.84L56 0l-8.46 5.93v2.18l-2.18 1.58v36.64h-8V24.07l-2.45-2.16v-2.82l-8.26-7.28-8.44 7.38V22L16 23.91v54.85H4V39.61h8v-4.08H0v47.32h207.54V35.53zM28.66 78.76h-1.95V17.28L28.66 19zm3.59 0h-2v-58.3l2 1.72zm3.67 0H34V23.7l2 1.72zm9.44 0h-8V59.5h8zm19.68 0H56v-10l9 6.57zm0-5.88L56 66.34v-3.43l9 6.54zM65 67l-9-6.54V57l9 6.56zm0-5.88l-9-6.55v-3.43l9 6.54zm0-5.89l-9-6.54v-3.44l9 6.55zm0-5.88l-9-6.54v-3.44l9 6.54zm0-5.88l-9-6.55v-3.43L65 40zm0-5.89L56 31v-3.4l9 6.54zm0-5.88l-9-6.55v-3.43l9 6.53zm0-5.89l-9-6.55v-3.43l9 6.54zm0-5.88l-9-6.55V10l9 6.48zM65 14l-9-6.5V4.07l9 6.53z" />
-              </svg>
-            </a>
-          </Link>
-        </div>
-        <div className="block sm:hidden">
-          <button className="flex items-center px-3 py-2">
-            <svg className="w-6 h-6 fill-current -mr-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+const Navbar = () => (
+  <div className="bg-blue-500 text-white">
+    <nav className="flex flex-wrap items-center justify-between w-4/5 py-8 md:py-12 mx-auto lg:w-3/5">
+      <div>
+        <Link href={'/'}>
+          <a>
+            <svg className="w-24 md:w-32 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 207.54 82.85">
+              <path d="M92.91 53A11.57 11.57 0 0084 49.43 11.51 11.51 0 0075.15 53a12.49 12.49 0 00-3.35 8.85 12.59 12.59 0 003.35 8.88A11.5 11.5 0 0084 74.37a11.6 11.6 0 008.9-3.59 12.59 12.59 0 003.34-8.91A12.48 12.48 0 0092.91 53zM89 67.51a6.64 6.64 0 01-10 0 8.72 8.72 0 01-1.85-5.64A8.61 8.61 0 0179 56.29a6.64 6.64 0 0110 0 8.55 8.55 0 011.85 5.58A8.66 8.66 0 0189 67.51zM102.5 68.27a6.18 6.18 0 001.55 4.52 6.11 6.11 0 004.55 1.58 6.87 6.87 0 004.87-1.53L112.24 69a3 3 0 01-2.31.83 2 2 0 01-1.67-.76 3.21 3.21 0 01-.59-2V54.54h4.82V50h-4.82v-6.5h-5.17V50h-3.94v4.52h3.94zM139.45 62.36a13.52 13.52 0 00-3.22-9.29 10.78 10.78 0 00-8.49-3.64 11.34 11.34 0 00-8.63 3.57 12.38 12.38 0 00-3.42 8.83 12 12 0 0012.44 12.5q6.06 0 9.64-3.45l-2.36-3.39a8.45 8.45 0 01-3.07 1.92 10.41 10.41 0 01-3.67.69 7.41 7.41 0 01-5.26-1.87 7.2 7.2 0 01-2.31-4.67h18.35zM121.05 60a7.2 7.2 0 011.95-4.45 6.1 6.1 0 014.77-1.89 6.18 6.18 0 014.89 1.92 6.8 6.8 0 011.8 4.38zM144.56 15.83h5.16v57.95h-5.16zM156.42 15.83h5.17v57.95h-5.17zM187.9 53a12.78 12.78 0 00-17.76 0 12.48 12.48 0 00-3.34 8.85 12.58 12.58 0 003.34 8.88 12.74 12.74 0 0017.76 0 12.6 12.6 0 003.35-8.91A12.49 12.49 0 00187.9 53zM184 67.51a6.14 6.14 0 01-5 2.29 6.07 6.07 0 01-5-2.29 8.66 8.66 0 01-1.85-5.64 8.55 8.55 0 011.85-5.58 6 6 0 015-2.29 6.11 6.11 0 015 2.29 8.61 8.61 0 011.85 5.58 8.72 8.72 0 01-1.85 5.64zM203.46 35.47h4.08v.06h-4.08zM71.8 35.53h66.07v4.08H71.8z" />
+              <path d="M168.27 35.53v4.08h35.19v39.15H66.34V10l-2.27-1.62V5.84L56 0l-8.46 5.93v2.18l-2.18 1.58v36.64h-8V24.07l-2.45-2.16v-2.82l-8.26-7.28-8.44 7.38V22L16 23.91v54.85H4V39.61h8v-4.08H0v47.32h207.54V35.53zM28.66 78.76h-1.95V17.28L28.66 19zm3.59 0h-2v-58.3l2 1.72zm3.67 0H34V23.7l2 1.72zm9.44 0h-8V59.5h8zm19.68 0H56v-10l9 6.57zm0-5.88L56 66.34v-3.43l9 6.54zM65 67l-9-6.54V57l9 6.56zm0-5.88l-9-6.55v-3.43l9 6.54zm0-5.89l-9-6.54v-3.44l9 6.55zm0-5.88l-9-6.54v-3.44l9 6.54zm0-5.88l-9-6.55v-3.43L65 40zm0-5.89L56 31v-3.4l9 6.54zm0-5.88l-9-6.55v-3.43l9 6.53zm0-5.89l-9-6.55v-3.43l9 6.54zm0-5.88l-9-6.55V10l9 6.48zM65 14l-9-6.5V4.07l9 6.53z" />
             </svg>
-          </button>
-        </div>
-        <div className="w-full slideup sm:flex sm:items-center sm:w-auto font-medium text-gray-200 space-x-8">
-          <Link href="/hotels">
-            <a className="hover:text-white">Hotels</a>
-          </Link>
-          <Link href="/about">
-            <a className="hover:text-white">About</a>
-          </Link>
-          <Link href="/contact">
-            <a className="hover:text-white">Contact</a>
-          </Link>
-          <div onClick={toggleModal} className="cursor-pointer hover:text-white">
-            Login/Sign Up
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
-};
+          </a>
+        </Link>
+      </div>
+      <div className="block sm:hidden">
+        <button className="flex items-center px-3 py-2">
+          <svg className="w-6 h-6 fill-current -mr-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div className="w-full slideup sm:flex sm:items-center sm:w-auto font-medium text-gray-200 space-x-8">
+        <Link href="/hotels">
+          <a className="hover:text-white">Hotels</a>
+        </Link>
+        <Link href="/about">
+          <a className="hover:text-white">About</a>
+        </Link>
+        <Link href="/contact">
+          <a className="hover:text-white">Contact</a>
+        </Link>
+        <LoginModal />
+      </div>
+    </nav>
+  </div>
+);
+
 export default Navbar;
