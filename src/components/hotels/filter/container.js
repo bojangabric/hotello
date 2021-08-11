b1c442f65c4a156bf89ae6a8891f59ddaeb93341
Bojan Gabric <bojan@bojangabric.com>
2021-08-11 12:04:42 +0200

    feat: add colapsing to filter containers

--- src/components/hotels/filter/container.js
+++ src/components/hotels/filter/container.js
@@ -1,13 +1,28 @@
-const FilterContainer = ({ name, children }) => (
-  <div className="py-4">
-    <div className="flex items-center justify-between text-blue-600 mb-2">
-      <div className="font-medium">{name}</div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 -mt-1 fill-current">
-        <path d="M15.3 10.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z" />
-      </svg>
+import { useState } from 'react';
+
+const FilterContainer = ({ name, children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <div className="py-4">
+      <div className="flex items-center justify-between text-blue-600 mb-2">
+        <div className="font-medium">{name}</div>
+        <svg
+          onClick={() => setCollapsed(!collapsed)}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-6 h-6 -mt-1 fill-current cursor-pointer"
+        >
+          <path d="M15.3 10.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z" />
+        </svg>
+      </div>
+      <div
+        className={`${collapsed && '!max-h-0'} transition-all ease-in duration-200 max-h-60 space-y-1 overflow-hidden`}
+      >
+        {children}
+      </div>
     </div>
-    <div className="space-y-1">{children}</div>
-  </div>
-);
+  );
+};
 
 export { FilterContainer };
