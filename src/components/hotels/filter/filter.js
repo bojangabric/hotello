126fdbbc9b4f7483b19f0db27e1f1279181afe1a
Bojan Gabric <bojan@bojangabric.com>
2021-08-11 12:05:10 +0200

    fix: show filter options on button click

--- src/components/hotels/filter/filter.js
+++ src/components/hotels/filter/filter.js
@@ -1,17 +1,21 @@
 import CheckBox from './checkbox';
 import { FilterContainer } from './container';
 
-const Filter = () => (
-  <div className="top-0 fixed inset-y-0 left-0 z-30 self-start w-64 px-4 mr-4 overflow-y-auto bg-white shadow-sm border-opacity-70 border-r lg:border border-gray-200 whitespace-nowrap lg:rounded lg:translate-x-0 lg:w-auto lg:overflow-visible lg:static lg:block">
+const Filter = ({ showFilter, setShowFilter }) => (
+  <div
+    className={`${
+      !showFilter && '-translate-x-full'
+    } top-0 transition transform fixed inset-y-0 left-0 z-30 self-start w-64 px-4 mr-4 overflow-y-auto bg-white shadow-sm border-opacity-70 border-r lg:border border-gray-200 whitespace-nowrap lg:rounded lg:translate-x-0 lg:w-auto lg:overflow-visible lg:static lg:block`}
+  >
     <div className="absolute top-0 left-0 pt-3 pl-2 lg:hidden">
-      <button className="block text-gray-600 hover:text-gray-800">
+      <button onClick={() => setShowFilter(false)} className="block text-gray-600 hover:text-gray-800">
         <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
           <path d="M17.293 18.707a1 1 0 001.414-1.414L13.414 12l5.293-5.293a1 1 0 00-1.414-1.414L12 10.586 6.707 5.293a1 1 0 00-1.414 1.414L10.586 12l-5.293 5.293a1 1 0 101.414 1.414L12 13.414l5.293 5.293z" />
         </svg>
       </button>
     </div>
-    <div className="text-xl font-medium text-gray-800 mt-3">Filter</div>
-    <div className="mt-8 lg:mt-0 divide-y">
+    <div className="text-xl font-medium text-gray-800 mt-12 lg:mt-3">Filter</div>
+    <div className="mt-0 divide-y">
       <FilterContainer name={'Hotel name'}>
         <input
           className="text-gray-600 mt-1 block w-full rounded border-gray-200 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 py-1"
--- src/components/hotels/hotels.js
+++ src/components/hotels/hotels.js
@@ -1,64 +1,72 @@
 import { Filter } from './filter';
 import Hotel from './hotel';
 import hotels from '../../database/hotels.json';
+import { useState } from 'react';
 
-const Hotels = () => (
-  <div className="flex justify-center py-32">
-    <Filter />
-    <div className="flex flex-col xl:w-3/5 lg:w-full lg:max-w-3xl">
-      <div className="items-center justify-between py-2 lg:flex">
-        <div className="text-lg font-medium text-gray-800">Showing 400 Hotels</div>
-        <div className="flex flex-col sm:flex-row sm:justify-between">
-          <button className="flex items-center justify-center px-6 py-2 my-4 text-white bg-blue-500 rounded lg:hidden">
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
-              <path d="M3 6a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM3 12a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM4 17a1 1 0 100 2h7a1 1 0 100-2H4z" />
+const Hotels = () => {
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <div className="flex justify-center py-32">
+      <Filter showFilter={showFilter} setShowFilter={setShowFilter} />
+      <div className="flex flex-col xl:w-3/5 lg:w-full lg:max-w-3xl">
+        <div className="items-center justify-between py-2 lg:flex">
+          <div className="text-lg font-medium text-gray-800">Showing 400 Hotels</div>
+          <div className="flex flex-col sm:flex-row sm:justify-between">
+            <button
+              onClick={() => setShowFilter(!showFilter)}
+              className="flex items-center justify-center px-6 py-2 my-4 text-white bg-blue-500 rounded lg:hidden"
+            >
+              <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
+                <path d="M3 6a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM3 12a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM4 17a1 1 0 100 2h7a1 1 0 100-2H4z" />
+              </svg>
+              Filter
+            </button>
+            <div className="flex">
+              <label className="flex items-center w-full text-gray-600">
+                <span className="w-20 mr-3 whitespace-nowrap">Sort by:</span>
+                <select className="block w-full mt-1 shadow-sm rounded border-gray-200 border-opacity-70 bg-white focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+                  <option>Popularity</option>
+                  <option>Price: Low to High</option>
+                  <option>Price: High to Low</option>
+                  <option>Highest Rating</option>
+                  <option>Most Reviews</option>
+                </select>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {hotels.map(hotel => (
+            <Hotel key={hotel.name} {...hotel} />
+          ))}
+        </div>
+
+        <div className="flex mx-auto text-blue-500">
+          <div className="flex items-center justify-center w-10 h-10 mr-2 text-gray-500 bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+              <path d="M13.7 15.3a1 1 0 01-1.4 1.4l-4-4a1 1 0 010-1.4l4-4a1 1 0 011.4 1.4L10.42 12l3.3 3.3z" />
             </svg>
-            Filter
-          </button>
-          <div className="flex">
-            <label className="flex items-center w-full text-gray-600">
-              <span className="w-20 mr-3 whitespace-nowrap">Sort by:</span>
-              <select className="block w-full mt-1 shadow-sm rounded border-gray-200 border-opacity-70 bg-white focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
-                <option>Popularity</option>
-                <option>Price: Low to High</option>
-                <option>Price: High to Low</option>
-                <option>Highest Rating</option>
-                <option>Most Reviews</option>
-              </select>
-            </label>
+          </div>
+          <div className="flex items-center justify-center w-10 h-10 mr-2 text-white bg-blue-500 border shadow-sm rounded border-gray-200 border-opacity-70">
+            1
+          </div>
+          <div className="flex items-center justify-center w-10 h-10 mr-2 font-medium bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
+            2
+          </div>
+          <div className="flex items-center justify-center w-10 h-10 font-medium bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
+            3
+          </div>
+          <div className="flex items-center justify-center w-10 h-10 ml-2 font-medium bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+              <path d="M10.3 8.7a1 1 0 011.4-1.4l4 4a1 1 0 010 1.4l-4 4a1 1 0 01-1.4-1.4l3.29-3.3-3.3-3.3z" />
+            </svg>
           </div>
         </div>
       </div>
-
-      <div>
-        {hotels.map(hotel => (
-          <Hotel key={hotel.name} {...hotel} />
-        ))}
-      </div>
-
-      <div className="flex mx-auto text-blue-500">
-        <div className="flex items-center justify-center w-10 h-10 mr-2 text-gray-500 bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-            <path d="M13.7 15.3a1 1 0 01-1.4 1.4l-4-4a1 1 0 010-1.4l4-4a1 1 0 011.4 1.4L10.42 12l3.3 3.3z" />
-          </svg>
-        </div>
-        <div className="flex items-center justify-center w-10 h-10 mr-2 text-white bg-blue-500 border shadow-sm rounded border-gray-200 border-opacity-70">
-          1
-        </div>
-        <div className="flex items-center justify-center w-10 h-10 mr-2 font-medium bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
-          2
-        </div>
-        <div className="flex items-center justify-center w-10 h-10 font-medium bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
-          3
-        </div>
-        <div className="flex items-center justify-center w-10 h-10 ml-2 font-medium bg-white border shadow-sm rounded border-gray-200 border-opacity-70">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-            <path d="M10.3 8.7a1 1 0 011.4-1.4l4 4a1 1 0 010 1.4l-4 4a1 1 0 01-1.4-1.4l3.29-3.3-3.3-3.3z" />
-          </svg>
-        </div>
-      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Hotels;
